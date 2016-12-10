49d216114596432d26db7c789aa25301be0dcac9
Niels Denissen <nielsdenissen@gmail.com>
2016-12-10 16:33:11 +0100

    Try calculating cipher using Caesar cipher.

--- code/decipher.py
+++ code/decipher.py
@@ -4,7 +4,19 @@ import requests
 
 
 def is_valid_sentence(sentence, language, acceptance_perc=0.8):
-    wordlist = sentence.split(" ")
+    """
+    Check if a sentence is valid in a certain language.
+
+    :param sentence: sentence to check
+    :param language: language to check sentence against
+    :param acceptance_perc: percentage of words that need to be correct
+    :return: True if valid words exceed acceptance percentage
+    """
+    # Remove all characters but words and lowercase all
+    processed_sentence = " ".join(re.findall("[a-zA-Z]+", sentence.lower()))
+
+    # Extract a list of words and check the percentage correct
+    wordlist = processed_sentence.split(" ")
     valid_word_perc = check_valid_word_perc(wordlist, language)
 
     return valid_word_perc >= acceptance_perc
@@ -34,35 +46,44 @@ def check_valid_word_perc(wordlist, language):
     return valid_word_perc
 
 
-def calc_cipher(text):
+def calc_cipher(text, language):
     """
     Calculate the cipher for text on input.
 
     :param text: encrypted text
+    :param language: language of text
     :return: cipher used to encrypt the text
     """
     # Get the processed text by removing any characters not in a-z and lowercase all
     processed_txt = " ".join(re.findall("[a-zA-Z]+", text.lower()))
+    decrypted_text = processed_txt
 
-    cipher = {
-        letter: letter for letter in string.ascii_lowercase
+    current_try = 0
+    while current_try < 100 and not is_valid_sentence(sentence=decrypted_text, language=language):
+        # Try a cipher
+        offset = current_try % 27
+        translate_from = string.ascii_lowercase
+        translate_to = string.ascii_lowercase[offset:] + string.ascii_lowercase[:offset]
+        cipher = {
+            l_from: l_to for l_from, l_to in zip(translate_from, translate_to)
         }
+
+        # Check if it works
+        decrypted_text = decipher_text(text=processed_txt, cipher=cipher)
+
+        current_try += 1
 
     return cipher
 
 
-def decipher_text(text, cipher=None):
+def decipher_text(text, cipher):
     """
-    Decipher the text on input.
-    If a cipher is given, use it to decipher the text. Otherwise, calculate the cipher.
+    Decipher the text on input using the cipher.
 
     :param text: encrypted text
     :param cipher: cipher to decrypt text with (optional)
     :return: decrypted text
     """
-    if cipher is None:
-        cipher = calc_cipher(text)
-
     translated_txt = ""
     for letter in text:
         if letter in string.ascii_lowercase:
--- test/test_decipher.py
+++ test/test_decipher.py
@@ -12,27 +12,30 @@ class TestDecipher(unittest.TestCase):
 
         test_cipher = {
             l_from: l_to for l_from, l_to in zip(translate_from, translate_to)
-            }
+        }
 
         encrypted_msg = decipher.decipher_text(test_msg, {v: k for k, v in test_cipher.items()})
 
-        self.assertEqual(test_msg, decipher.decipher_text(text=encrypted_msg, cipher=test_cipher))
+        self.assertEqual(
+            test_msg,
+            decipher.decipher_text(text=encrypted_msg, cipher=test_cipher)
+        )
 
 
 class TestWordCheck(unittest.TestCase):
     def test_word_existence_dutch(self):
         # Dutch
         test_words = ['test', 'frieten', 'gek', 'jjjjj']
-        self.assertEqual(decipher.check_valid_word_perc(test_words, 'nld'), 0.75)
+        self.assertEqual(decipher.check_valid_word_perc(wordlist=test_words, language='nld'), 0.75)
 
     def test_valid_sentence_dutch(self):
         test_sentence = "dit is een test bericht"
-        self.assertTrue(decipher.is_valid_sentence(test_sentence, 'nld'))
+        self.assertTrue(decipher.is_valid_sentence(sentence=test_sentence, language='nld'))
 
     def test_word_existence_english(self):
         # English
         test_words = ['test', 'fries', 'freaky', 'jjjjj']
-        self.assertEqual(decipher.check_valid_word_perc(test_words, 'eng'), 0.75)
+        self.assertEqual(decipher.check_valid_word_perc(wordlist=test_words, language='eng'), 0.75)
 
 
 class TestFindCypher(unittest.TestCase):
@@ -45,8 +48,9 @@ class TestFindCypher(unittest.TestCase):
         test_msg = self.dutch_msg
         test_cipher = {letter: letter for letter in string.ascii_lowercase}
 
-        self.assertEqual(test_msg, decipher.decipher_text(test_msg))
-        self.assertDictEqual(test_cipher, decipher.calc_cipher(test_msg))
+        found_cipher = decipher.calc_cipher(text=test_msg, language='nld')
+        self.assertDictEqual(test_cipher, found_cipher)
+        self.assertEqual(test_msg, decipher.decipher_text(text=test_msg, cipher=found_cipher))
 
     def test_simple_cipher_dutch(self):
         test_msg = self.dutch_msg
@@ -59,8 +63,9 @@ class TestFindCypher(unittest.TestCase):
             }
         encrypted_msg = decipher.decipher_text(test_msg, {v: k for k, v in test_cipher.items()})
 
-        self.assertEqual(test_msg, decipher.decipher_text(encrypted_msg))
-        self.assertDictEqual(test_cipher, decipher.calc_cipher(encrypted_msg))
+        found_cipher = decipher.calc_cipher(text=encrypted_msg, language='nld')
+        self.assertDictEqual(test_cipher, found_cipher)
+        self.assertEqual(test_msg, decipher.decipher_text(text=encrypted_msg, cipher=found_cipher))
 
 
 if __name__ == '__main__':
