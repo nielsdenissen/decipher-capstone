832cfedacaace69fb3b009dc79fb320e276f3a42
Niels Denissen <nielsdenissen@gmail.com>
2016-12-10 20:54:35 +0100

    Check all permutations for decrypting

--- code/decipher.py
+++ code/decipher.py
@@ -1,5 +1,7 @@
 import string
 import re
+import itertools
+import time
 from code import validation
 
 
@@ -13,23 +15,23 @@ def calc_cipher(text, language):
     """
     # Get the processed text by removing any characters not in a-z and lowercase all
     processed_txt = " ".join(re.findall("[a-zA-Z]+", text.lower()))
-    decrypted_text = ""
 
-    current_try = 0
-    while current_try < 100 and \
-            not validation.is_valid_sentence(sentence=decrypted_text, language=language):
+    all_ascii_permutations = itertools.permutations(list(string.ascii_lowercase), 26)
+    start_time = time.time()
+    for permutation in all_ascii_permutations:
         # Try a cipher
-        offset = current_try % 27
         translate_from = string.ascii_lowercase
-        translate_to = string.ascii_lowercase[offset:] + string.ascii_lowercase[:offset]
+        translate_to = permutation
         cipher = {
             l_from: l_to for l_from, l_to in zip(translate_from, translate_to)
-            }
+        }
 
         # Check if it works
         decrypted_text = decipher_text(text=processed_txt, cipher=cipher)
 
-        current_try += 1
+        if validation.is_valid_sentence(sentence=decrypted_text, language=language) or \
+                time.time() - start_time > 30:
+            break
 
     return cipher
 
