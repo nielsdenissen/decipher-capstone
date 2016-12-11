60e921dbe286990ee4fe29c565f604665f09d802
Niels Denissen <nielsdenissen@gmail.com>
2016-12-11 17:17:04 +0100

    Test decipher for multiple languages. Move testing code to generic test function.

--- test/test_decipher.py
+++ test/test_decipher.py
@@ -5,29 +5,57 @@ from code import decipher, translator
 
 class TestFindCypher(unittest.TestCase):
     def setUp(self):
-        # Make a default message to translate
-        self.dutch_msg = "Hallo allemaal, " \
-                         "dit is een test bericht om te kijken hoe goed dit algoritme werkt."
+        """
+        Create a default message for each language to test the deciphering with
+        """
+        self.test_lang_msgs = {
+            'nl': "Hallo allemaal, " \
+                  "dit is een test bericht om te kijken hoe goed dit algoritme werkt.",
+            'en': "Hey everyone, this is a test message to see if how well this algorithm is doing."
+        }
 
-    def test_no_cipher_dutch(self):
-        test_msg = self.dutch_msg
-        test_cipher = {letter: letter for letter in string.ascii_lowercase}
+    def _generic_decryption_test(self, test_lang_msgs, target_cipher):
+        """
+        Generic test whether decryption works as expected. Tests whether deciphering the
+        encrypted message for a language results in the expected target_msg and target_cipher.
 
-        found_cipher = decipher.calc_cipher(text=test_msg, language='nl')
-        self.assertDictEqual(test_cipher, found_cipher)
-        self.assertEqual(test_msg, translator.decipher_text(text=test_msg, cipher=found_cipher))
+        :param target_cipher: Cipher to use on messages and try to find
+        :return: Fails if deciphering doesn't work
+        """
+        # Run through all languages and test messages
+        for language, original_msg in test_lang_msgs.items():
+            # Encrypt the message
+            encrypted_msg = translator.encipher_text(original_msg, target_cipher)
 
-    def test_simple_cipher_dutch(self):
-        test_msg = self.dutch_msg
+            # Calculate the cipher and decipher the message with it
+            found_cipher = decipher.calc_cipher(text=encrypted_msg, language=language)
+            found_msg = translator.decipher_text(text=encrypted_msg, cipher=found_cipher)
 
+            # Check against targets
+            self.assertDictEqual(target_cipher, found_cipher)
+            self.assertEqual(original_msg, found_msg)
+
+    def test_no_cipher(self):
+        """
+        Test a cipher that maps letters to itself
+        """
+        # Create a cipher with each letter mapped to itself
+        test_cipher = translator.create_cipher(string.ascii_lowercase)
+
+        self._generic_decryption_test(test_lang_msgs=self.test_lang_msgs,
+                                      target_cipher=test_cipher)
+
+    def test_caesar_cipher(self):
+        """
+        Test a caesar cipher (shifts all letters with a certain offset)
+        """
+        # Create a caesar cipher
+        caesar_offset = 3
         test_cipher = translator.create_cipher(
-            string.ascii_lowercase[1:] + string.ascii_lowercase[0])
+            string.ascii_lowercase[caesar_offset:] + string.ascii_lowercase[:caesar_offset])
 
-        encrypted_msg = translator.encipher_text(test_msg, test_cipher)
-
-        found_cipher = decipher.calc_cipher(text=encrypted_msg, language='nl')
-        self.assertDictEqual(test_cipher, found_cipher)
-        self.assertEqual(test_msg, translator.decipher_text(text=encrypted_msg, cipher=found_cipher))
+        self._generic_decryption_test(test_lang_msgs=self.test_lang_msgs,
+                                      target_cipher=test_cipher)
 
 
 if __name__ == '__main__':
